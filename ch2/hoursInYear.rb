3ea61d745987b885a19bd66d8575c223305b8cfd
Huw-Man <hu.newman@berkeley.edu>
2017-05-29 23:20:02 -0600

    revised hoursInYear and minutesInDecade

--- ch2/hoursInYear.rb
+++ ch2/hoursInYear.rb
@@ -2,3 +2,4 @@ hrs = 24
 days = 365
 
 puts hrs*days
+puts "#{hrs*366} hrs in a leap year"
--- ch2/minutesInDecade.rb
+++ ch2/minutesInDecade.rb
@@ -3,4 +3,5 @@ hours = 24
 days = 365
 year = 10
 
-puts min*hours*days*year
+puts "some decades have 2 or 3 leap years"
+puts "#{min*hours*days*year + min*hours*2} to #{min*hours*days*year + min*hours*3} minutes in a decade"

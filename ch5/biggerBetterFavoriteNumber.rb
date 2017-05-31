65cfeea5cdd806dbb0c3cd2b4e948b704916e9ec
Huw-Man <hu.newman@berkeley.edu>
2017-05-30 22:17:20 -0600

    finished ch5 assignments

---
+++ ch5/biggerBetterFavoriteNumber.rb
@@ -0,0 +1,7 @@
+puts "What is your favorite number?
+I will return a bigger and better number that should be your new favorite number"
+
+fnum = (gets.chomp).to_i
+newNum = fnum +1
+
+puts newNum.to_s+" is your Bigger Better Favorite number."
---
+++ ch5/fullNameGreeting.rb
@@ -0,0 +1,12 @@
+puts "Hello, what is your name?"
+puts "What is your first name?"
+fname = gets.chomp
+
+puts "What is your middle name?"
+mname = gets.chomp
+
+puts "What is your last name?"
+lname = gets.chomp
+
+puts "Hi "+fname+" "+mname+" "+lname+" it's a pleasure to meet you."
+puts "What a fantastics name! :P"

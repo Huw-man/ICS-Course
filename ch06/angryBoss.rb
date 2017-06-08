3ac63592a42bd607487c94dabf18fc7f908f1baa
Huw-Man <hu.newman@berkeley.edu>
2017-06-07 17:08:27 -0700

    added chapter 6 assignments

---
+++ ch06/angryBoss.rb
@@ -0,0 +1,3 @@
+puts "HEY! WHAT DO YOU WANT!"
+want = gets.chomp
+puts "WHAT DO YOU MEAN \""+ want + "\"?!! \nYOUR FIRED!"
---
+++ ch06/tableOfContents.rb
@@ -0,0 +1,7 @@
+lw = 60 #line width
+
+puts "Table of Contents".center(lw)
+puts "\n"
+puts "Chapter 1: Getting Started".ljust(lw/2) + "page 1".rjust(lw/2)
+puts "Chapter 2: Numbers".ljust(lw/2) + "page 9".rjust(lw/2)
+puts "Chapter 3: Letters".ljust(lw/2) + "page 13".rjust(lw/2)

72923e17d2031b1887808ec52fb5a519a3320f75
Huw-Man <hu.newman@berkeley.edu>
2017-06-16 10:29:09 -0700

    revised tableOfContents

--- ch08/tableOfContents.rb
+++ ch08/tableOfContents.rb
@@ -1,12 +1,22 @@
-lw = 60 #line width
-
-table = ["Table of Contents".center(lw),
-  "\n",
-  "Chapter 1: Getting Started".ljust(lw/2) + "page 1".rjust(lw/2),
-  "Chapter 2: Numbers".ljust(lw/2) + "page 9".rjust(lw/2),
-  "Chapter 3: Letters".ljust(lw/2) + "page 13".rjust(lw/2)
+table = [["Table of Contents",2],
+["\n\n",0],
+["Chapter 1: Getting Started", 1],
+["page 1", 3],
+["Chapter 2: Numbers", 1],
+["page 9", 3],
+["Chapter 3: Letters", 1],
+["page 13", 3]
 ]
 
+lw = 60 #line width
 table.each do |t|
-  puts t
+  if t[1]== 1
+    print t[0].ljust(lw/2)
+  elsif t[1]== 2
+    print t[0].center(lw)
+  elsif t[1]== 3
+    print t[0].rjust(lw/2) +"\n"
+  elsif t[1]==0
+    print t[0]
+  end
 end
